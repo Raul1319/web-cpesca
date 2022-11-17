c240e6be3668786c2a38d0c8c155745d7b631950
johndoe@example.com <johndoe@example.com>
2022-11-17 11:00:36 +0000

    Ruta Editar Profile FE done

--- src/pages/ProfileEdit.jsx
+++ src/pages/ProfileEdit.jsx
@@ -1,20 +1,126 @@
 import React from 'react'
-import { useState } from "react"
-import { editPorfileSevice } from "../services/profile.services";
+import { useState, useEffect, useContext } from "react"
+import { editPorfileSevice, profileService, deleteProfileService } from "../services/profile.services";
 import { useNavigate, useParams, Link } from "react-router-dom"
+import {AuthContext} from "../context/auth.context"
+import CircleLoader from "react-spinners/CircleLoader";
 
 
 function ProfileEdit() {
 
-  const navigate = useNavigate()
+  const navigate = useNavigate();
+  const {user} = useContext(AuthContext);
 
-  const {iduser} = useParams()
-  
+  const {userId} = useParams();
+  const {profileId} = useParams();
+
+  const [details, setDetails] = useState(null);
+  const [isFetching, setIsFetching] = useState(true);
+
+  const [usernameInput, setUsernameInput] = useState("");
+  const [emailInput, setEmailInput] = useState("");
+
+  useEffect(() =>{
+    getData()
+
+  }, [])
+
+  const getData = async () =>{
+
+    try {
+
+      const response = await profileService();
+      setDetails(response.data);
+      setUsernameInput(response.data.username);
+      setEmailInput(response.data.email);
+       
+      setIsFetching(false);
+    } catch (error) {
+      navigate("/error");
+      
+    }
+
+  }
+
+   const handleUsernameChange = (e) => setUsernameInput(e.target.value);
+   const handleEmailChange = (e) => setEmailInput(e.target.value);
+
+   const handleEditProfile = async (e) => {
+
+    e.preventDefaul();
+
+    const editProfile = {
+
+      username: usernameInput,
+      email: emailInput
+    }
+
+    try {
+
+      await  editPorfileSevice( profileId, editProfile)
+      
+      navigate("/profile")
+    } catch (error) {
+      navigate("/error")
+      
+    }
+
+   }
+
+   if(isFetching === true) {
+    return <CircleLoader />
+   }
+
+   const handleDeleteProfile = async () => {
+
+    try {
+
+      await deleteProfileService(userId)
+
+      navigate("/singunp")
+      
+      
+    } catch (error) {
+      navigate("/error")
+      
+    }
+   }
+
+   
   return (
     <div>
       
       <h2>Edita tu Perfil</h2>
+      <br />
+      <h3>Bienvenido {details.username}</h3>
+      <br />
+      <form>
+      <label htmlFor="username">Usuario</label>
+      <input type="text" name="username" value={usernameInput} onChange={handleUsernameChange}></input>
+      <br />
+      <label htmlFor="email">Email:</label>
+      <input type="email" name="email" value={emailInput} onChange={handleEmailChange}></input>
+      <br />
+
+
+      <button onClick={handleEditProfile}>Actualizar</button>
       
+      <div>
+
+       <button onClick={handleDeleteProfile}>Dar de Baja</button>
+
+      </div>
+      
+
+
+
+
+
+
+
+
+
+      </form>
       
       
        </div>
